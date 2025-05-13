28d52d3d82cd6c46abeb23c72ae7c75a68b978ae
Periato <rm562117@fiap.com.br>
2025-05-13 19:34:55 -0300

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Pedro Miranda RM: 562117
+// Pedro Luis RM:564441
+// Israel RM: 563435
 
-int main() {
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Função de Fibonacci
+void fibonacci() {
+    int N;
+    int vetor[50];
+
+    printf("Digite a quantidade de termos da sequencia de Fibonacci (1 a 50): ");
+    scanf("%d", &N);
+
+    if (N < 1 || N > 50) {
+        printf("Número inválido. Tente novamente.\n");
+        return;
+    }
+
+    vetor[0] = 0;
+    if (N > 1) vetor[1] = 1;
+
+    for (int i = 2; i < N; i++) {
+        vetor[i] = vetor[i - 1] + vetor[i - 2];
+    }
+
+    printf("Sequencia de Fibonacci: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+}
+
+// Função para calcular fatorial
+int fatorial(int n) {
+    int fat = 1;
+    for (int i = 2; i <= n; i++) {
+        fat *= i;
+    }
+    return fat;
+}
+
+// Função para mostrar os fatoriais de 1 a N
+void mostrar_fatoriais() {
+    int N;
+    do {
+        printf("Digite um numero inteiro entre 1 e 20: ");
+        scanf("%d", &N);
+        if (N < 1 || N > 20) {
+            printf("Entrada inválida. Tente novamente.\n");
+        }
+    } while (N < 1 || N > 20);
+
+    for (int i = 1; i <= N; i++) {
+        printf("%d! = %d\n", i, fatorial(i));
+    }
+}
+void verificar_palindromo() {
     char palavra[101];
     int i, tamanho, palindromo = 1;
 
@@ -27,5 +84,59 @@ int main() {
         printf("A palavra \"%s\" nao e um palindromo.\n", palavra);
     }
 
+    return ;
+}
+void verificarSubstring() {
+    char str1[101], str2[101];
+
+    printf("Digite a primeira string: ");
+    scanf("%s", str1);
+    printf("Digite a segunda string: ");
+    scanf("%s", str2);
+
+    if (strstr(str1, str2) != NULL)
+        printf("A segunda string esta contida na primeira.\n");
+    else
+        printf("A segunda string Nao esta contida na primeira.\n");
+}
+
+// Menu de opções
+void menu() {
+    char opcao;
+    do {
+        printf("\n===== MENU DE EXERCICIOS =====\n");
+        printf("1 - Sequencia de Fibonacci\n");
+        printf("2 - Fatoriais\n");
+        printf("3 - Verificar Palindromo\n");
+        printf("4 - Verificar Substring\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf(" %c", &opcao);
+
+        switch (opcao) {
+            case '1':
+                fibonacci();
+                break;
+            case '2':
+                mostrar_fatoriais();
+                break;
+            case '3':
+                verificar_palindromo();
+                break;
+            case '4':
+                verificarSubstring();
+                break;
+            case '0':
+                printf("Encerrando o programa.\n");
+                break;
+            default:
+                printf("Opção invalida.\n");
+        }
+    } while (opcao != '0');
+}
+
+// Função principal
+int main() {
+    menu(); // Chama o menu
     return 0;
 }

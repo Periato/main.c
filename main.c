9a798f895232186066308c7f85cd7cb701451a64
Periato <rm562117@fiap.com.br>
2025-05-14 10:06:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -50,34 +50,7 @@ void mostrar_fatoriais() {
     }
 }
 
-void verificar_palindromo() {
-    char palavra[101];
-    int i, tamanho, palindromo = 1;
 
-    printf("Digite uma palavra (sem espacos, ate 100 caracteres): ");
-    scanf("%100s", palavra);
-
-    tamanho = strlen(palavra);
-
-    for (i = 0; i < tamanho / 2; i++) {
-        char caractereInicio = palavra[i];
-        char caractereFim = palavra[tamanho - 1 - i];
-
-        if (caractereInicio != caractereFim) {
-            palindromo = 0;
-            break;
-        }
-    }
-
-
-    if (palindromo) {
-        printf("A palavra \"%s\" e um palindromo.\n", palavra);
-    } else {
-        printf("A palavra \"%s\" nao e um palindromo.\n", palavra);
-    }
-
-    return ;
-}
 void verificarSubstring() {
     char str1[101], str2[101];
 
